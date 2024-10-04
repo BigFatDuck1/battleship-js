1c90e0fa6c5756f462b6052b7842246cb8355c2b
BigFatDuck1 <clementho999@gmail.com>
2024-10-05 01:46:42 +0800

    Backend: Gameboard and Ship class nearly finished, Player class not yet started

--- battleship.js
+++ battleship.js
@@ -1,13 +1,17 @@
 
 
 class Ship {
-    constructor(length, start, orientation) { //start in the form of coordinates [x,y]
+    constructor(length, start, orientation="horizontal") { //start in the form of coordinates [x,y]
         this.length = length;
         this.hits = 0;
         this.sunk = false;
         this.start = start; //[x,y]
         this.orientation = orientation; //horizontal or vertical
-        this.coordinates = [];
+        this.coordinates = []; //Graphics coordinates system: 0,0 is at the top-left corner (not cartesian plane)
+    }
+
+    accessCoordinates () {
+        return this.coordinates;
     }
 
     hit() {
@@ -25,14 +29,16 @@ class Ship {
     }
 
     setCoordinates(){
+        let start_x = this.start[0];
+        let start_y = this.start[1];
         if (this.orientation == "vertical") {
             for (let i = 0; i < this.length; i++) {
-                this.coordinates.push([start[0], start[1] + i]);
+                this.coordinates.push([start_x, start_y + i]);
             }
         }
         else if (this.orientation == "horizontal") {
             for (let i = 0; i < this.length; i++) {
-                this.coordinates.push([start[0] + i, start[1]]);
+                this.coordinates.push([start_x + i, start_y]);
             }
         }
 
@@ -53,15 +59,21 @@ class Ship {
 }
 
 class Gameboard {
-    constructor(dimensions) {
+    constructor(dimensions=10) {
         this.dimensions = dimensions;
-        this.board = [];
+        this.board = {};
         for (let i = 0; i < dimensions; i++) {
             for (let j = 0; j < dimensions; j++) {
-                this.board.push([i,j]);
+                let new_coordinate = `${i},${j}`;
+                this.board[new_coordinate] = {
+                    coordinates: [i,j],
+                    status : "safe"
+                };
             }
         }
         this.ship_dict = [];
+        this.number_of_ships = 5; //Each side has 5 ships
+        this.ships_sunk = 0;
     }
 
     newShip(x,y,length, orientation) { //Ship(length, start, orientation)
@@ -77,6 +89,49 @@ class Gameboard {
         //Takes coordinates, determines whether attack hit any ship
         //calls Ship.hit() to appropriate ship
         //records coordinate of the hit
+
+        //Access coordinate in this.board and changes status to "hit", if status is already "hit", return
+        let coordinate = `${x},${y}`;
+        if (this.board[coordinate].status == "hit") {
+            return 1;
+        }
+        else {
+            this.callHitOnTargetShip([x,y]);
+            this.board[coordinate].status = "hit";
+        }
+
+    }
+
+    callHitOnTargetShip(coords) { //coords in an array [x,y]
+        this.ship_dict.forEach((ship) => {
+            let ship_coordinates_array = ship.accessCoordinates();
+            ship_coordinates_array.forEach((coordinate) => {
+                if (coordinate[0] == coords[0] && coordinate[1] == coords[1]) {
+                    ship.hit();
+                    //Check if sunk
+                    let ship_isSunk = ship.isSunk();
+                    if (ship_isSunk == true) {
+                        //Log number of sunken ships
+                        this.ships_sunk += 1;
+                        if (this.ships_sunk == this.number_of_ships) {
+                            this.gameOver();
+                        }
+                    }
+                    return ship;
+                }
+            });
+            
+        })
+    }
+    
+    gameOver() {
+        //TODO: Game over function
+        return "Game over!";
+    }
+
+    logBoard() {
+        console.log(this.board);
+        return this.board;
     }
 
 }
--- battleship.test.js
+++ battleship.test.js
@@ -1,7 +1,16 @@
 const { Ship, Gameboard, Player } = require("./battleship");
 
-const myShip = new Ship(3)
+const myShip = new Ship(3, [0,0], "vertical");
+const gb = new Gameboard(10);
 
 test("Show health of ship", () => {
     expect(myShip.showHealth()).toBe(3);
 });
+
+// test("Access board in Gameboard class", () => {
+//     expect(gb.logBoard()).toBe(gb.board);
+// })
+
+test("Create new ship and return coordinates", () => {
+    expect(myShip.setCoordinates()).toEqual([[0,0], [0,1], [0,2]]);
+})
